3e2f37089e6307e6824f360db939f8c2d8390ee2
Josh Burlile <josh.burlile@gmail.com>
2022-04-15 12:01:02 -0700

    ZooKeeperPart1

---
+++ Week2/Extra Practice/.metadata/.plugins/org.eclipse.core.resources/.history/97/d022968956bc001c192cb918e8c276fc
@@ -0,0 +1,12 @@
+package zookeeperpart1;
+
+public class MammalTest {
+
+	public static void main(String[] args) {
+		
+		Mammal giraffe = new Mammal(100);
+		System.out.println(giraffe.displayEnergy());
+
+	}
+
+}
---
+++ Week2/Extra Practice/ZooKeeperPart1/src/zookeeperpart1/GorillaTest.java
@@ -0,0 +1,18 @@
+package zookeeperpart1;
+
+public class GorillaTest {
+
+	public static void main(String[] args) {
+		
+		Gorilla chad = new Gorilla(100);
+		chad.throwSomething();
+		chad.throwSomething();
+		chad.throwSomething();
+		chad.eatBananas();
+		chad.eatBananas();
+		chad.climb();
+	
+
+	}
+
+}
