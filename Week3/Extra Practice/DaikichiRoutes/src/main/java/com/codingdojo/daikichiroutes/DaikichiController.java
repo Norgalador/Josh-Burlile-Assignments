8f3eff50f5af28cee28f196f80424c6d8d647f09
Josh Burlile <josh.burlile@gmail.com>
2022-04-18 16:36:03 -0700

    Daikichi Routes

---
+++ Week3/Extra Practice/DaikichiRoutes/src/main/java/com/codingdojo/daikichiroutes/DaikichiController.java
@@ -0,0 +1,26 @@
+package com.codingdojo.daikichiroutes;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+@RequestMapping("/daikichi")
+public class DaikichiController {
+	
+	@RequestMapping("")
+	public String index() {
+		return "Welcome!";
+	}
+	
+	@RequestMapping("/today")
+	public String luck() {
+		return "Today you will find luck in all your endeavors!";
+	}
+	
+	@RequestMapping("/tomorrow")
+	public String fortune() {
+		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+}
